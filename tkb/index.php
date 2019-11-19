1428245da7101ff2a7f6f1d1d0cbc2da488fe8da
ngthuc <lenguyenthuc.edu@gmail.com>
2019-11-19 22:51:23 +0700

    Add page

---
+++ tkb/index.php
@@ -0,0 +1,63 @@
+<html>
+<title>TKB</title>
+<head>
+<link rel="shortcut icon" href="http://lapvo3.com/tintuc/files/news/thumb/1274518253.nv_15.png" type="image/x-icon" />
+<link rel="icon" href="http://lapvo3.com/tintuc/files/news/thumb/1274518253.nv_15.png" type="image/x-icon" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script language="JavaScript" type="text/javascript">
+function go(){
+if (document.selecter.select1.options[document.selecter.select1.selectedIndex].value != "none") 
+{document.getElementById('f1').src = document.selecter.select1.options[document.selecter.select1.selectedIndex].value}
+}
+</script>
+</head>
+
+
+<body>
+
+
+<div style="position: absolute; width: 570px; height: 20px; z-index: 8; left: 0px; top: 17px" id="layer13">
+	<font color="#FFFFFF">
+<form name="selecter"><select name="select1" size="1">
+<option value="frame1">Chọn ngày
+
+<option value=none>--------------------
+<option value="290713.htm">29/07/2013
+<option value="120813.htm">12/08/2013
+<option value="190813.htm">19/08/2013
+<option value="020913.htm">02/09/2013
+<option value="090913.htm">09/09/2013
+<option value="300913.htm">30/09/2013
+<option value="071013.htm">07/10/2013
+<option value="181113.htm">18/11/2013
+<option value="021213.htm">02/12/2013
+<option value="231213.htm">23/12/2013
+<option value="301213.htm">30/12/2013
+<option value="060114.htm">06/01/2014
+<option value="130114.htm">13/01/2014
+<option value="170214.htm">17/02/2014
+<option value="030314.htm">03/03/2014
+<option value="140314.htm">17/03/2014
+<option value="240314.htm">24/03/2014
+<option value="140414.htm">14/04/2014
+<option value="210414.htm">21/04/2014
+<option value="280414.htm">28/04/2014
+<option value="end.htm">Now - Hiện nay
+
+</select>
+<input type="button" value="Xem TKB" onclick="go()">
+</form>
+	</font></div>
+
+
+
+<div style="position: absolute; width: 570px; height: 500px; z-index: 15; left: 0px; top: 47px" id="layer20">
+<iframe name="f1" src="frame1.html" width="580" height="400" id="f1" style="height: 100%; width: 100%; float: left" marginwidth="10" marginheight="10" border="0" frameborder="0">
+			Your browser does not support inline frames or is currently configured not to display inline frames.
+			</iframe></div>
+		
+		</p>
+
+</body>
+
+</html>
